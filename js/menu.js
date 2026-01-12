844c8c34f9ca35449628ce14deb0320f6e6258f3
EL670 <50767829+EL670@users.noreply.github.com>
2026-01-12 13:52:38 +0100

    Playing around JS

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,11 @@ $(document).ready(function() {
         $(this).children("a").toggleClass("closed open"); // wechselt beim Klick auf "dt" die Klasse des enthaltenen a-Tags von "closed" zu "open".
     });
 });
+
+function displayDate(){
+    document.getElementById("datetest").innerHTML = Date();
+
+}
+
+const d = new Date();
+document.getElementByID("date2").innerHTML =d;
